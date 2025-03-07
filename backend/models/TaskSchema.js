731a7d98f484f7897d698fef590a2ef2472a8d11
小野瑛人 <eito@Eitos-MacBook-Air.local>
2025-03-07 18:43:31 +0900

    フォルダーの構成を変更

---
+++ backend/models/TaskSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+      default: 0,
+    },
+    understanding: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+    subjectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subject",
+      required: true,
+    },
+  },
+  { collection: "tasks" }
+);
+
+module.exports = mongoose.model("Task", TaskSchema);
